9a1103de4a63526f6f76e56b851f6dbe2fbdbecc
Mehmet Baris POLAT <mehmetpolat@akinon.com>
2024-02-02 22:25:07 +0000

    [MBP-1] Added web socket example

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Messaging Service
+
+## Setup
+
+### Required:
+
+- Docker
+
+### Step by Step
+
+- Create network in docker
+
+```
+docker network create messagingservicenet
+```
+
+- Add Dev Container extension into Visual Studio Code
+
+```
+Name: Dev Containers
+Id: ms-vscode-remote.remote-containers
+Description: Open any folder or repository inside a Docker container and take advantage of Visual Studio Code's full feature set.
+Version: 0.330.0
+Publisher: Microsoft
+VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers
+```
+
+- Open command palette by using CTRL + P or F1. Write `Dev Containers: Open Folder in Container`.
+
+- Create virtual environment after finishing installing container.
+
+```
+python -m venv .venv
+```
+
+- Activate environment
+
+```
+source .venv/bin/activate
+```
+
+- Install packages
+
+```
+pip install --editable .
+```
+
+- Run
+
+```
+messaging-service
+```
--- src/messaging_service/__init__.py
+++ src/messaging_service/__init__.py
@@ -1,2 +1,43 @@
+import uvicorn
+
+from functools import partial
+from fastapi import FastAPI, WebSocket
+
+from .api import get, websocket_endpoint
+
+
+app = FastAPI()
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
 def main():
-    pass
+
+    app.get("/")(get)
+
+    manager = ConnectionManager()
+    # TODO: Add manager as parameter to websocket_endpoint by using partial
+    app.websocket("/ws/{client_id}")(partial(websocket_endpoint, manager=manager))
+
+    uvicorn.run("messaging_service:app", host="0.0.0.0", port=5000, log_level="info")
+
+
+if __name__ == "__main__":
+    main()
